7c4aa1d6ea6369e64400e5313fc7479e341094ba
unknown <aishwaryanalwade2017@gmail.com>
2024-07-11 15:39:35 +0530

    added another method to add data

--- src/main/java/com/example/git_demo/controller/gitcontroller.java
+++ src/main/java/com/example/git_demo/controller/gitcontroller.java
@@ -1,6 +1,7 @@
 package com.example.git_demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,13 @@ public class gitcontroller {
     @GetMapping("/hellogit")
     public String hellogit() {
         return "Hello, git-World!";
+    }
+
+    @GetMapping("/adddata")
+    public String addData(@RequestParam(required = false) String data) {
+        if (data != null && data.equals("aishwarya")) {
+            return "hello again";
+        }
+        return "data received: " + data;
     }
 }
